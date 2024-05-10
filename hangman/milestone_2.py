be08f5a778f6cb2290008bb111cf2587028502be
ChrisLeon7 <152982506+ChrisLeon7@users.noreply.github.com>
2024-05-10 20:26:58 +0100

    Creation of fucntions

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,12 +2,31 @@ import random
 
 word_list = ["peach", "mango", "blackberry", "coconut", "grapefruit"]
 word = random.choice(word_list)
+letter = True
 
-guess = input("Guess a letter\n")
 
-if guess == 1:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input")
 
-    
+
+
+
+
+
+def check_guess(guess):
+    guess = guess.lower
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")   
+
+def ask_for_input():
+    if len(guess) == 1:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+    while True:
+        guess = input("Guess a letter\n").lower
+        if guess.isalpha() == True:
+            False
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
